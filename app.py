a06bda5903ac0528c0e759552da24af5a983c36e
Amanuel Larebo <77553304+alarebo1@users.noreply.github.com>
2023-01-27 18:21:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user,current_user, LoginManager,UserM
 import songinfo
 from flask_sqlalchemy import SQLAlchemy
 app = flask.Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DTBASEURL
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/test.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = '5115secretekey'
 
