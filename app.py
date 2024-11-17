5f1d680afbf13a225d588f9f4b66fccc39f0d85b
Amanuel Larebo <77553304+alarebo1@users.noreply.github.com>
2024-11-17 00:00:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ db = SQLAlchemy(app)
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL").replace(
 #     "://", "ql://", 1
 # )
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 login_manager = LoginManager()
