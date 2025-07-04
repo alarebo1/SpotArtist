2fc3b22c0c53b977114d330b0b32d4631b8e8487
Amanuel Larebo <77553304+alarebo1@users.noreply.github.com>
2025-07-03 20:36:27 -0400

    Update app.py
    changing db to postgress

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from flask_login import login_user, logout_user,current_user, LoginManager,UserM
 import songinfo
 from flask_sqlalchemy import SQLAlchemy
 app = flask.Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-
+db_url = os.getenv("DATABASE_URL")
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = os.getenv("secretkey")
 
